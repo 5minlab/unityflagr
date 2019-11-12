621d3edd365beb918c152b8cc587a2e1ab49a352
Kim Shihwan <shihwan1004@naver.com>
2019-11-12 15:25:08 +0900

    make wrapper for postEvaluation

--- Program.cs
+++ Program.cs
@@ -1,39 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 
-using Nozomi;
+using Newtonsoft.Json;
 
 namespace testtest
 {
+    class EvalContext
+    {
+        public string entityID;
+        public string entityType;
+        public Dictionary<string, object> entityContext;
+        public bool enableDebug;
+        public int? flagID;
+        public string flagKey;
+    }
+
+    class EvalBatchReq
+    {
+        public List<Entity> entities;
+        public bool enableDebug;
+        public List<int> flagIDs;
+        public List<string> flagKeys;
+    }
+
+    class Entity
+    {
+        public string entityID;
+        public string entityType;
+        public Dictionary<string, object> entityContext;
+    }
+
+    class EvalBatchResult
+    {
+        public List<EvalResult> evaluationResults;
+    }
+
+    class EvalResult
+    {
+        public int flagID;
+        public string flagKey;
+        public int flagSnapshotID;
+        public int segmentID;
+        public int variantID;
+        public string variantKey;
+        public Dictionary<string, object> variantAttachment;
+        public EvalContext evalContext;
+        public string timestamp;
+    }
+
     class Program
     {
-        public string baseAddress;
+        public static readonly HttpClient client = new HttpClient();
 
-        public readonly HttpClient client;
-        public IContentSerializer requestSerializer;
-        public IContentSerializer responseSerializer;
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("asdf");
+            var req = new EvalContext();
+            req.entityID = "127";
+            req.entityType = "user";
+            req.entityContext = new Dictionary<string, object>(){
+                {"state", "NY"}
+            };
+            // req.flagID = 1;
+            req.flagKey = "kmmcd1nsd6ze56chh";
+            req.enableDebug = true;
+
+
+            var content = await SerializeAsync(req);
+            var resp = await client.PostAsync("https://try-flagr.herokuapp.com/api/v1/evaluation", content);
+            var respContent = resp.Content;
+            var result = await DeserializeAsync<EvalResult>(respContent);
+            Console.WriteLine(JsonConvert.SerializeObject(result));
         }
 
-        public Program(HttpClient client, string baseAddress, IContentSerializer requestSerializer, IContentSerializer responseSerializer)
-        {
-            this.client = client ?? throw new ArgumentNullException(nameof(client));
-            this.baseAddress = baseAddress ?? throw new ArgumentNullException(nameof(baseAddress));
-            this.requestSerializer = requestSerializer ?? throw new ArgumentNullException(nameof(requestSerializer));
-            this.responseSerializer = responseSerializer ?? throw new ArgumentNullException(nameof(responseSerializer));
-        }
 
-        public async Task<TResp> Handle<TReq, TResp>(string method, string path, TReq req, CancellationToken cancellationToken)
+        public static Task<HttpContent> SerializeAsync<T>(T data)
         {
-            using (var message = new HttpRequestMessage(new HttpMethod(method), $"{baseAddress}{path}"))
-            using (var httpReq = new SimpleRequest(client, message, cancellationToken, requestSerializer, responseSerializer))
+            if (data == null)
             {
-                return await httpReq.Request<TReq, TResp>(req);
+                return null;
+            }
+
+            string json;
+
+            using (var writer = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                var _serializer = JsonSerializer.Create();
+                _serializer.Serialize(jsonWriter, data);
+                jsonWriter.Flush();
+
+                json = writer.ToString();
+            }
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            return Task.FromResult<HttpContent>(content);
+        }
+
+        public static async Task<T> DeserializeAsync<T>(HttpContent content)
+        {
+            var _serializer = JsonSerializer.Create();
+            using (var s = await content.ReadAsStreamAsync().ConfigureAwait(false))
+            using (var sr = new StreamReader(s))
+            {
+                return (T)_serializer.Deserialize(sr, typeof(T));
             }
         }
     }
